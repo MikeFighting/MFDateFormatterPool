1eb556df44ab492b74719398312847904da6ad25
Mike <huangzhihui@58ganji.com>
2017-08-27 16:15:17 +0800

    init the date formatter pool

---
+++ MFDateFormatterPool.h
@@ -0,0 +1,81 @@
+//
+//  MFDateFormatterPool.h
+//  BornToTry
+//
+//  Created by Mike on 27/08/2017.
+//  Copyright © 2017 itemei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFDateFormatterPool : NSObject
+
++ (instancetype)sharedInstance;
+
+/**
+ Get data formatter by format string and local;
+ 
+ @param format the format string
+ @param locale the NSLocale instance
+ @return data formatter
+ */
+- (NSDateFormatter *)dateFormatterWithFormat:(NSString *)format andLocale:(NSLocale *)locale;
+
+/**
+ Get data formatter by format string and local identifier;
+ 
+ @param format  the format string
+ @param localeIdentifier the local identifier
+ @return data formatter
+ */
+- (NSDateFormatter *)dateFormatterWithFormat:(NSString *)format andLocaleIdentifier:(NSString *)localeIdentifier;
+
+/**
+ Get data formatter by format string, the default local is [NSLocale currentLocale]
+ 
+ @param format Format string
+ @return data formatter
+ */
+- (NSDateFormatter *)dateFormatterWithFormat:(NSString *)format;
+
+/*
+ typedef NS_ENUM(NSUInteger, NSDateFormatterStyle) {    // date and time format styles
+ NSDateFormatterNoStyle = kCFDateFormatterNoStyle,
+ NSDateFormatterShortStyle = kCFDateFormatterShortStyle,
+ NSDateFormatterMediumStyle = kCFDateFormatterMediumStyle,
+ NSDateFormatterLongStyle = kCFDateFormatterLongStyle,
+ NSDateFormatterFullStyle = kCFDateFormatterFullStyle
+ };
+ */
+
+/**
+ Get data formatter by date style, time style and local
+
+ @param dateStyle date style
+ @param timeStyle time style
+ @param locale the NSLocale instance
+ @return date formatter
+ */
+- (NSDateFormatter *)dateFormatterWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle andLocal:(NSLocale *)locale;
+
+/**
+ Get data formatter by date style, time style and local
+ 
+ @param dateStyle date style
+ @param timeStyle time style
+ @param locale the NSLocale identifier
+ @return date formatter
+ */
+- (NSDateFormatter *)dateFormatterWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle andLocalIdentifer:(NSString *)localeIdentifier;
+
+/**
+ Get data formatter by date style, time style and  defalut local is [NSLocale currentLocale]
+ 
+ @param dateStyle date style
+ @param timeStyle time style
+ @return date formatter
+ */
+- (NSDateFormatter *)dateFormatterWithDateStyle:(NSDateFormatterStyle)dateStyle andTimeStyle:(NSDateFormatterStyle)timeStyle;
+
+
+@end
