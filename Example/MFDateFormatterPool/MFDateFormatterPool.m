206fb94c895012ac9662d8d5bccc1ead88ff468a
Mike <huangzhihui@58ganji.com>
2018-12-16 23:18:28 +0800

    use the global queue to enhance the performance

--- Example/MFDateFormatterPool/MFDateFormatterPool.m
+++ Example/MFDateFormatterPool/MFDateFormatterPool.m
@@ -13,17 +13,14 @@
 
 @property (nonatomic, strong) NSMutableDictionary *cachedDateFormatters;
 
-
 @end
 
-static dispatch_semaphore_t _cachePoolLock;
+static dispatch_queue_t _syncQueue;
 
-static void _MFCachePoolInitGlobal(){
-
+static void _MFCachePoolInitGlobal() {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        
-        _cachePoolLock = dispatch_semaphore_create(1);
+        _syncQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
         
     });
 }
@@ -36,7 +33,6 @@ static void _MFCachePoolInitGlobal(){
     static MFDateFormatterPool *dataFormatter = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        
         
         dataFormatter = [[MFDateFormatterPool alloc]init];
         NSCache *dataFormatterCache = [[NSCache alloc]init];
@@ -77,11 +73,9 @@ static void _MFCachePoolInitGlobal(){
     dateFormatter = [[NSDateFormatter alloc]init];
     dateFormatter.dateFormat = format;
     dateFormatter.locale = locale ?: [NSLocale currentLocale];
-    [self p_storeDateFormatter:dateFormatter forKey:formatterKey];
+    [self p_creatDateFormatWithFormat:dateFormatter forKey:formatterKey];
     return dateFormatter;
 }
-
-
 
 - (NSDateFormatter *)dateFormatterWithFormat:(NSString *)format {
 
@@ -96,8 +90,6 @@ static void _MFCachePoolInitGlobal(){
 - (NSDateFormatter *)dateFormatterWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle andLocal:(NSLocale *)locale {
 
         NSAssert(locale, @"MFDateFormatterPool.m: locale == nil. You can use dateFormatterWithDateStyle:andTimeStyle:, if you want to use the current local");
-
-    
         NSString *formatterKey = [self p_getChaeKeyWithDateStyle:dateStyle timeStyle:timeStyle andLocalIdentifier:locale.localeIdentifier];
         NSDateFormatter *dateFormatter = [self p_getDateFormatterByKey:formatterKey];
         if (dateFormatter != nil) {
@@ -107,10 +99,8 @@ static void _MFCachePoolInitGlobal(){
         dateFormatter.dateStyle = dateStyle;
         dateFormatter.timeStyle = timeStyle;
         dateFormatter.locale = locale;
-        [self p_storeDateFormatter:dateFormatter forKey:formatterKey];
+        [self p_creatDateFormatWithFormat:dateFormatter forKey:formatterKey];
         return dateFormatter;
-
-
 }
 
 
@@ -124,28 +114,27 @@ static void _MFCachePoolInitGlobal(){
 - (NSString *)p_getCacheKeyWithFormat:(NSString *)format andIdentifier:(NSString *)localIdentifier {
     
     return [NSString stringWithFormat:@"%@|%@",format, localIdentifier];
-    
 }
 
 - (NSString *)p_getChaeKeyWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle andLocalIdentifier:(NSString *)localIdentifier {
 
     return [NSString stringWithFormat:@"%lu.%lu|%@",dateStyle,timeStyle,localIdentifier];
-    
 }
 
 - (NSDateFormatter *)p_getDateFormatterByKey:(NSString *)key {
     
-    dispatch_semaphore_wait(_cachePoolLock, DISPATCH_TIME_FOREVER);
-    NSDateFormatter *dateFormatter = [self.cachedDateFormatters objectForKey:key];
-    dispatch_semaphore_signal(_cachePoolLock);
-    return  dateFormatter;
+    __block NSDateFormatter *dateFormatter;
+    dispatch_sync(_syncQueue, ^{
+        dateFormatter = [self.cachedDateFormatters objectForKey:key];
+    });
+    return dateFormatter;
 }
 
-- (void)p_storeDateFormatter:(NSDateFormatter *)dateFormatter forKey:(NSString *)key {
+- (void)p_creatDateFormatWithFormat:(NSDateFormatter *)dateFormatter forKey:(NSString *)key {
     
-    dispatch_semaphore_wait(_cachePoolLock, DISPATCH_TIME_FOREVER);
-    [self.cachedDateFormatters setObject:dateFormatter forKey:key];
-    dispatch_semaphore_signal(_cachePoolLock);
+    dispatch_barrier_async(_syncQueue, ^{
+        [self.cachedDateFormatters setObject:dateFormatter forKey:key];
+    });
 }
 
 @end
